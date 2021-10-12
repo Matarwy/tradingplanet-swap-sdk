b2367ab8501319054753244ab11abb37f3acf5fe
elhassan elmatarwy <elhassan214@gmail.com>
2021-10-12 13:49:13 +0200

    change token address

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -55,7 +55,7 @@ export class Pair {
       tokenAmounts[0].token.chainId,
       Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token),
       18,
-      'PLANET-LP',
+      'TP-LP',
       'TradingPlanet LPs'
     )
     this.tokenAmounts = tokenAmounts as [TokenAmount, TokenAmount]
